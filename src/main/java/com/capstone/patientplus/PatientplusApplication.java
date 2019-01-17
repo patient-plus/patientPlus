eaa4a0fe3a1ed9c39473ed577e89e61120e69ff6
MattBrozusky <matthew.brozusky@gmail.com>
2019-01-17 13:50:20 -0600

    first commit

---
+++ src/main/java/com/capstone/patientplus/PatientplusApplication.java
@@ -0,0 +1,14 @@
+package com.capstone.patientplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientplusApplication.class, args);
+    }
+
+}
+
