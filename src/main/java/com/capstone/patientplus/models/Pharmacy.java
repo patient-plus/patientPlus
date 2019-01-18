62d13ea9f5a868d09795f0f6915129bf00de7655
Maria Vela <maria.vela790@gmail.com>
2019-01-18 11:44:42 -0600

    added pharma model

---
+++ src/main/java/com/capstone/patientplus/models/Pharmacy.java
@@ -0,0 +1,66 @@
+package com.capstone.patientplus.models;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "pharmacies")
+public class Pharmacy {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotBlank(message = "pharmacy name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pharmacy")
+    private List<User> patients;
+
+    //@OneTo
+
+    public Pharmacy(){}
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
