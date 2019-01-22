8b8f54894fab6061f5ef28ad81acabc48358f79d
Maria Vela <maria.vela790@gmail.com>
2019-01-21 20:30:13 -0600

    setting up doctor service

--- src/main/java/com/capstone/patientplus/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.patientplus.repositories;
-
-import com.capstone.patientplus.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
