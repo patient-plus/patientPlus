2638e70a3f8eec669ae79c05e46023c8a3ed8a05
Maria Vela <maria.vela790@gmail.com>
2019-01-22 14:33:40 -0600

    refactored the controllers to works as services

--- src/main/java/com/capstone/patientplus/repositories/AppointmentRepository.java
+++ src/main/java/com/capstone/patientplus/repositories/AppointmentRepository.java
@@ -4,6 +4,7 @@ package com.capstone.patientplus.repositories;
 import com.capstone.patientplus.models.Appointment;
 import com.capstone.patientplus.models.DoctorPatient;
 
+import com.capstone.patientplus.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.print.Doc;
@@ -11,6 +12,6 @@ import java.util.List;
 
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
 
-    List<Appointment> findByPatient(DoctorPatient patient);
-    List<Appointment> findByDoctor(DoctorPatient doctor);
+    List<Appointment> findByPatient(User patient);
+    List<Appointment> findByDoctor(User doctor);
 }
--- src/main/java/com/capstone/patientplus/services/DoctorService.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.patientplus.services;
-
-public class DoctorService {
-//    doctors need to be able to:
-// -grab all appointments they have
-// -view patient profiles and paperwork
-//-verify account
-}
