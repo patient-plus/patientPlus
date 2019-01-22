6586b8d4d34ac9723f1a3140b27d2919f6050a68
Maria Vela <maria.vela790@gmail.com>
2019-01-22 10:05:39 -0600

    merge fix

--- src/main/java/com/capstone/patientplus/repositories/AppointmentRepository.java
+++ src/main/java/com/capstone/patientplus/repositories/AppointmentRepository.java
@@ -6,8 +6,10 @@ import com.capstone.patientplus.models.DoctorPatient;
 
 import org.springframework.data.repository.CrudRepository;
 
+import javax.print.Doc;
 import java.util.List;
 
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
 
+    Appointment findByCombination(DoctorPatient combination);
 }
