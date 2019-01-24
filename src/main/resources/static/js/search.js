0d8b1f1c3e641c70c629896b4663c5f4dd6fec23
Maria Vela <maria.vela790@gmail.com>
2019-01-24 14:37:06 -0600

    set up search url

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -3,16 +3,21 @@
 $(document).ready(function(){
     //test for connection between files
     // $('#background').css('background-color', 'yellow');
+    let api_key = ``;
+
     let options = $(`#insurances`);
     let plans = $(`#plans`);
     let searchBtn = $(`#search-btn`);
-    let results = $(`#results-list`);
+    let firstName = $(`#doctor-first-name`);
+    let lastName = $(`#doctor-last-name`);
+    let location = $(`#location`);
+    let resultsSection = $(`#results-list`);
 
     options.empty();
-    options.append('<option selected="true" disable>Choose Insurance Provider</option>');
+    options.append('<option selected="true" disable value="choose-provider">Choose Insurance Provider</option>');
 
     plans.empty();
-    plans.append('<option selected="true">Choose Plan</option>');
+    plans.append('<option selected="true" value="choose-plan">Choose Plan</option>');
 
     const request = () =>{
         // return fetch(`https://api.betterdoctor.com/2016-03-01/insurances?user_key=`)
@@ -57,11 +62,32 @@ $(document).ready(function(){
 
     });
 
-    searchBtn.click(
-        request().then((data) => {
-        //    add functionality to bring up results and populate the list
-        })
-    )
+    searchBtn.click( (e) => {
+        e.preventDefault();
+        var requestUrl = 'https://api.betterdoctor.com/2016-03-01/doctors?';
+
+        if (options.val() !== "choose-provider" && plans.val() !== "choose-plan") {
+            requestUrl += `insurance_uid=${plans.val()}&`;
+        }
+        if(firstName.val() !== ''){
+            requestUrl += `first_name=${firstName.val()}&`;
+        }
+        if(lastName.val() !== ''){
+            requestUrl += `last_name=${lastName.val()}&`;
+        }
+        if(location.val() !== ''){
+            console.log(location.val().trim());
+            requestUrl += `location=${location.val()}&`;
+        }
+        requestUrl += `user_key=${api_key}`;
+            console.log(requestUrl);
+
+        // request().then((data) => {
+        //
+        // //    add functionality to bring up results and populate the list
+        // })
+
+    })
 
 
 });
