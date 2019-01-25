8e0697edc687d44f10046e32a0aafbd8bc0330e8
Maria Vela <maria.vela790@gmail.com>
2019-01-25 14:33:16 -0600

    going back to original controller

--- src/main/java/com/capstone/patientplus/controllers/TestController.java
+++ src/main/java/com/capstone/patientplus/controllers/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/find-doctor")
+    @GetMapping("/search")
     public String getSearchPage(){
         return "search";
     }
