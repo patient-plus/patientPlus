d99109b75ada16eee16f28711dbfa2f435126ca1
Maria Vela <maria.vela790@gmail.com>
2019-02-06 14:34:37 -0600

    made medical info mobile

--- src/main/resources/static/js/medicalFormMobile.js
+++ src/main/resources/static/js/medicalFormMobile.js
@@ -1,22 +1,3 @@
 $(document).ready(() => {
 
-    let sections = ['ec', 'insurance', 'pharmacy', 'surgeries', 'medications'];
-    let sectionBtns = ['insurance', 'pharmacy', 'surgeries', 'medications'];
-
-
-    for(let i = 0; i < sections.length; i++){
-        $(`#drop-down-menu-${i}`).on('click', (e) => {
-            e.preventDefault(); //prevents submitting button
-            $(`#collapsible-${sections[i]}`).toggleClass('d-none');
-        });
-    }
-
-
-    for(let i = 0 ; i < sectionBtns.length ; i++){
-        $(`#to-${sectionBtns[i]}`).on('click', (e) => {
-            e.preventDefault();
-            $(`#collapsible-${sections[i]}`).toggleClass('d-none');
-            $(`#collapsible-${sections[i + 1]}`).toggleClass('d-none');
-        });
-    }
 });
--- src/main/resources/static/js/nav-footer.js
+++ src/main/resources/static/js/nav-footer.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
     let counter = 1;
+    let sections = ['ec', 'insurance', 'pharmacy', 'surgeries', 'medications'];
+    let sectionBtns = ['insurance', 'pharmacy', 'surgeries', 'medications'];
 
     $('.dropdown').click(function () {
         (counter++) % 2 === 1 ? $(this).next().removeClass('d-none') : $(this).next().addClass('d-none');
@@ -10,4 +12,26 @@ $(document).ready(function () {
         $('footer').css('position', $(document).height() > $(window).height() ? "inherit" : "fixed");
     }
     footerPosition();
+
+
+
+
+    for(let i = 0; i < sections.length; i++){
+        $(`#drop-down-menu-${i}`).on('click', (e) => {
+            e.preventDefault(); //prevents submitting button
+            $(`#collapsible-${sections[i]}`).toggleClass('d-none');
+            footerPosition();
+        });
+    }
+
+
+    for(let i = 0 ; i < sectionBtns.length ; i++){
+        $(`#to-${sectionBtns[i]}`).on('click', (e) => {
+            e.preventDefault();
+            if($(`#`))
+            $(`#collapsible-${sections[i]}`).toggleClass('d-none');
+            $(`#collapsible-${sections[i + 1]}`).toggleClass('d-none');
+            footerPosition();
+        });
+    }
 });
